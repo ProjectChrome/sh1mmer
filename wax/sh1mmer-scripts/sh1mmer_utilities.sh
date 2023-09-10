2ada199d52421a5fa0b95c4b27b71d1347f56304
elliothegamer2 <127466749+elliothegamer2@users.noreply.github.com>
2023-09-09 23:52:36 -0500

    Update sh1mmer_utilities.sh

--- wax/sh1mmer-scripts/sh1mmer_utilities.sh
+++ wax/sh1mmer-scripts/sh1mmer_utilities.sh
@@ -2,12 +2,18 @@ source /usr/sbin/sh1mmer_gui.sh
 source /usr/sbin/sh1mmer_optionsSelector.sh
 
 deprovision() {
-    vpd -i RW_VPD -s check_enrollment=0
-    unblock_devmode
+    echo "You now Need WP Disable or this wont work"
+    vpd -l
+    echo "SUPER IMPORTANT WRITE DOWN YOUR SERIAL NUMBER"
+    echo "Also look on the back of your chromebook you can find it there BUT STILL WRITE IT DOWN"
+    read -p "Press Enter to delete Serial Number"
+    vpd -d serial_number
 }
 
 reprovision() {
-    vpd -i RW_VPD -s check_enrollment=1
+    echo "Hope you know your serial number LOL"
+    read -p "Please Type your serial numbert> " $serialnum
+    vpd -s serial_number="$serialnum"
 }
 
 usb() {
