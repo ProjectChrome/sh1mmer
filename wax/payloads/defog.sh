65edcdc78b259bcb7f81380b26ced3d20e67dc6f
elliothegamer2 <127466749+elliothegamer2@users.noreply.github.com>
2023-09-11 16:15:02 -0500

    Update defog.sh

--- wax/payloads/defog.sh
+++ wax/payloads/defog.sh
@@ -6,5 +6,6 @@ fi
 futility gbb --flash -s --flags=0x8090
 crossystem block_devmode=0
 vpd -i RW_VPD block_devmode=0
+clear
 echo "GBB flags set. Devmode should now be unblocked"
 read -p "Press enter to continue" t
